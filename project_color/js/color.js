73a748952b3418b39de5499c6e0a9a7b543168aa
Pedro Gomez <pandresgomezp@gmail.com>
2021-05-01 23:47:03 -0500

    Color project

---
+++ project_color/js/color.js
@@ -0,0 +1,11 @@
+const color = document.querySelector("#color")
+const salida = document.querySelector("#salida")
+
+color.addEventListener("input", () => {
+    const value = color.value
+
+    salida.innerHTML = value
+    salida.style.background = value
+    salida.style.color = "#FFFFFF"
+})
+
