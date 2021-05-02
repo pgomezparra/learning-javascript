8985dc82aa475c74cf67fcafb94b8ebe2f4ab243
Pedro Gomez <pandresgomezp@gmail.com>
2021-05-01 23:47:21 -0500

    Matrix project

---
+++ project_matrix/js/main.dev.js
@@ -0,0 +1,33 @@
+const element = document.querySelector("#canvas")
+
+const ctx = element.getContext("2d")
+
+const w = document.body.clientWidth
+const h = document.body.clientHeight
+
+element.width = w
+element.height = h
+
+const position = Array(300).join(0).split('')
+
+const initMatrix = () => {
+    ctx.fillStyle = "rgba(0, 15, 2, 0.15)"
+    ctx.fillRect(0,0,w,h)
+
+    ctx.fillStyle = '#4caf50'
+    ctx.font = '11pt'
+
+    position.map(
+        (y, index) => {
+            const text = String.fromCharCode(1e3 + Math.random() * 30)
+            const x = index * 15 + 15
+
+            canvas.getContext('2d').fillText(text, x, y)
+
+            y > 100 + Math.random() * 1e5 ? (position[index] = 0) : (position[index] = y + 15)
+        }
+    )
+}
+
+setInterval(() => initMatrix(), 50)
+
